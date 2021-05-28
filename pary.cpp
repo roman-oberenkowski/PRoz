e0ad0d5e058eea388ecad9ad3a6eaad571fa5a3d
Grecjanin <kacperleski@gmail.com>
2021-05-28 14:57:43 +0200

    Dodanie działającego prototypu sekcji krytycznej

--- pary.cpp
+++ pary.cpp
@@ -20,6 +20,9 @@ using namespace std;
 #define FINISH 1
 #define APP_MSG 2
 #define INV 3
+#define REQforS 101
+#define ACKSection 102
+#define RELofS 103
 
 int thread_rank,size;
 int lamportClock = 10;
@@ -35,8 +38,19 @@ typedef struct
     int clock; /* można zmienić nazwę na bardziej pasujące */
 } packet_t;
 
+
+typedef struct
+{
+    int id;
+    int clock;
+}queueEl;
+std::vector <queueEl> queueForRoom;
+int receivedACKS;
+pthread_mutex_t roomMut = PTHREAD_MUTEX_INITIALIZER;
+int S = 1;
+
 string getTabs(){
-    string a="";
+    string a=to_string(thread_rank)+" ";
     for(int i=0;i<thread_rank;i++){
         a+='\t';
     }
@@ -86,13 +100,182 @@ void send_finish_to_all(){
     }
 }
 
+string queue2Str(std::vector <queueEl> queue)
+{
+    string res = "";
+    string pom;
+    std::vector<queueEl>::iterator it;
+        for(it = queue.begin() ; it<queue.end() ; it++)
+        {
+            pom = to_string(it->id);
+            pom += ":";
+            pom +=to_string(it->clock);
+            res += pom+" , ";
+        }
+    return res;
+}
+
+void insertIntoQueue(std::vector <queueEl>* queue , queueEl el)
+{
+    cout<<getTabs()<<"wstawiam do kolejki"<<endl;
+    if(queue->size() == 0)queue->push_back(el);
+    else{
+        bool inserted = false;
+        std::vector<queueEl>::iterator it;
+        for(it = queue->begin() ; it<queue->end() ; it++)
+        {
+            if(el.clock < it->clock)
+            {
+                queue->insert(it ,el);
+                inserted = true;
+                break;
+            }
+            else if(el.clock == it->clock && el.id < it->id)
+            {
+                queue->insert(it ,el);
+                inserted = true;
+                break;
+            }
+        }
+        if(inserted == false)queue->push_back(el);
+    }
+    cout<<getTabs()<<"wstawiłem do kolejki"<<endl;
+    cout<<getTabs()<<queue2Str(*queue)<<endl;
+}
+
+void sendReqForRoom(int partnerId = 0)
+{
+    cout<<getTabs()<<"wysłam REQ"<<endl;
+    queueEl el;
+    int rank,size;
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    MPI_Comm_size(MPI_COMM_WORLD, &size);
+    el.id = rank;
+    packet_t packet;
+    packet.appdata = partnerId;
+	pthread_mutex_lock(&clockMut);
+    lamportClock++;
+    packet.clock = lamportClock;
+    for(int i=0;i<size;i++)
+    {
+        if(i!= rank)
+        {
+            MPI_Send(&packet , 1 , MPI_PAKIET_T , i , REQforS,MPI_COMM_WORLD);
+        }
+    }
+    el.clock = lamportClock;
+    insertIntoQueue(&queueForRoom , el);
+    pthread_mutex_unlock(&clockMut);
+    cout<<getTabs()<<"wysłałem REQ"<<endl;
+    
+}
+
+void resp2Req(packet_t packet , MPI_Status status , std::vector <queueEl>* queue)
+{
+    cout<<getTabs()<<"REQforRoom from "<<status.MPI_SOURCE<<" c:"<<packet.clock<<endl;
+    queueEl el;
+    el.id = status.MPI_SOURCE;
+    el.clock = packet.clock;
+    insertIntoQueue(queue , el);
+    sendMsg(0 , 1, MPI_PAKIET_T , status.MPI_SOURCE , ACKSection);
+    cout<<getTabs()<<"odpowiedziałem na REQ"<<endl;
+}
+void resp2Rel( packet_t packet ,MPI_Status status , std::vector <queueEl> *queue)
+{
+    cout<<getTabs()<<"RelOfRoom from "<<status.MPI_SOURCE<<" c:"<<packet.clock<<endl;
+    std::vector<queueEl>::iterator it;
+    for(it = queue->begin() ; it<queue->end() ; it++)
+    {
+          if(it->id == status.MPI_SOURCE)
+          {
+              queue->erase(it);
+              break;
+          }  
+    }
+    cout<<getTabs()<<"kolejka: "<<queue2Str(*queue)<<endl;
+    cout<<getTabs()<<"obsłużyłem REL"<<endl;
+}
+
+bool check4Section(std::vector <queueEl>* queue , int* confirmed, int sectionSize)
+{
+    std::cout<<getTabs()<<"Sprawdzam sekcje, liczba acków: "<<*confirmed<<std::endl;
+    bool res = false;
+    if(*confirmed == size-1)
+    {
+        std::cout<<getTabs()<<"sostałem wszytski acki, rozmiar kolejki:"<<queue->size()<<std::endl;
+        if(!queue->empty())
+        {
+            std::cout<<getTabs()<<"kolejka nie jest pusta"<<std::endl;
+            int pom = queue->size();
+            for(int i=0; i< std::min(sectionSize , pom);i++)
+            {
+                if((queue->begin()+i)->id == thread_rank)
+                {
+                    res = true;
+                    *confirmed = 0;
+                    pthread_mutex_unlock(&roomMut);
+                    break;
+                }
+            }
+            // if(queueForRoom.front().id == thread_rank)
+            // {
+            //     res = true;
+            //     receivedACKS = 0;
+            //     pthread_mutex_unlock(&roomMut);
+                
+            // }
+        }
+        
+    }
+
+    
+
+    return res;
+}
+void leaveQueue(std::vector <queueEl>* queue)
+{
+    for(int i=0; i<queue->size();i++)
+            {
+                if((queue->begin()+i)->id == thread_rank)
+                {
+                    queue->erase(queue->begin()+i);
+                    break;
+                }
+            }
+}
 /* Kod funkcji wykonywanej przez wątek */
 void *mainThreadFunc(void *ptr)
 {
     packet_t pakiet;
-    sendMsg(965, 1, MPI_PAKIET_T, 2, INV);
-    sleep(5);
-    send_finish_to_all();
+    if(thread_rank != 0)
+    {
+        //sleep(rand()%5);
+        sendReqForRoom();
+        pthread_mutex_lock(&roomMut);
+        std::cout<<getTabs()<<"Hura jest w sekcji krytycznej"<<std::endl;
+        sleep(20);
+        std::cout<<getTabs()<<"Opuszczam sekcję"<<std::endl;
+        leaveQueue(&queueForRoom);
+        packet_t packet;
+        pthread_mutex_lock(&clockMut);
+        lamportClock++;
+        packet.clock = lamportClock;
+        for(int i=0;i<size;i++)
+        {
+            if(i!= thread_rank)
+            {
+                MPI_Send(&packet , 1 , MPI_PAKIET_T , i , RELofS,MPI_COMM_WORLD);
+            }
+        }
+        pthread_mutex_unlock(&clockMut);
+
+    }
+    
+    
+
+    //sendMsg(965, 1, MPI_PAKIET_T, 2, INV);
+    //sleep(5);
+    //send_finish_to_all();
     return 0;
 }
 
@@ -111,15 +294,18 @@ int main(int argc, char **argv)
     MPI_Type_create_struct(nitems, blocklengths, offsets, typy, &MPI_PAKIET_T);
     MPI_Type_commit(&MPI_PAKIET_T);
 
+    pthread_mutex_lock(&roomMut);
+
     pthread_t threadA;
     pthread_create(&threadA, NULL, mainThreadFunc, 0);
     char end = FALSE;
     
     cout<<getTabs()<<"started "<<thread_rank<<endl;
     srand((int)time(0)+thread_rank);
-    
+
     packet_t pakiet;
     MPI_Status status;
+    receivedACKS = 0;
     while (!end)
     {
         recvMsg(&pakiet, 1, MPI_PAKIET_T, MPI_ANY_SOURCE, MPI_ANY_TAG, MPI_COMM_WORLD, &status);
@@ -132,6 +318,18 @@ int main(int argc, char **argv)
             break;
         case INV:
             process_INV(pakiet,status,thread_rank);
+            break;
+        case RELofS:
+            resp2Rel(pakiet, status , &queueForRoom);
+            check4Section(&queueForRoom , &receivedACKS, S);
+            break;
+        case REQforS:
+            resp2Req(pakiet , status , &queueForRoom);
+            break;
+        case ACKSection:
+            cout<<getTabs()<<"dostałem ACK, source:"<<status.MPI_SOURCE<<endl;
+            receivedACKS += 1;
+            check4Section(&queueForRoom , &receivedACKS , S);
             break;
         default:
             break;
